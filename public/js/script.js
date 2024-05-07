22bd95a05687206440812797d46715b98d25f74e
Jaymar Westerlow <jaywesterlow71@gmail.com>
2024-05-08 00:12:23 +0200

    Create partial - Turn radio into star

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,22 @@
 const menuBtn = document.getElementById("menu-btn");
 const menuContent = document.getElementById("menu-content");
 const body = document.querySelector('body');
+const favButton = document.querySelectorAll('.heart')
 
-window.onload=function(){
+window.onload=function() {
   menuBtn.addEventListener("click", openMenu);
 }
 
-function openMenu(){
+function openMenu() {
   menuBtn.classList.toggle('active')
   menuContent.classList.toggle('menuOpen')
   body.classList.toggle('maxHeight')
+}
+
+for (let i = 0; i < favButton.length; i++) {
+  // Voeg een event listener toe aan elke knop
+  favButton[i].addEventListener('click', function() {
+      // Toggle de class bij het klikken op de knop
+      this.classList.toggle('heart-favorite');
+  });
 }
